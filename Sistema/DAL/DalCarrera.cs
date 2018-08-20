905e261c62585cc5bc974334e653ca92a1e9172f
GabrielWH824 <41213963+GabrielWH824@users.noreply.github.com>
2018-08-19 22:40:54 -0300

    Algo de carrera
    estoy haciendo lo de materia ahora

---
+++ Sistema/DAL/DalCarrera.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.DS;
+
+namespace Sistema.DAL
+
+{
+    class DALCarrera
+    {
+        DSCarrera dscar = new DSCarrera();
+
+        public void CargarCarrera (string bNombre, int bAños, string bNºResolucion, int bHorarios)
+        {
+            dscar.Carrera.Rows[0]["Carrera"] = Convert.ToString(bNombre);
+            dscar.Carrera.Rows[0]["Año"] = Convert.ToInt32(bAños);
+            dscar.Carrera.Rows[0]["Nº Resolucion"] = Convert.ToString(bNºResolucion);
+            dscar.Carrera.Rows[0]["Horas"] = Convert.ToInt32(bHorarios);
+
+            try
+            {
+                ConexionBD.StoredSinDatos( $"GuardarCarrera + '{dscar.Carrera.Rows[0]["Carrera"]}' ,'{dscar.Carrera.Rows[1]["Carrera"]}',  '{dscar.Carrera.Rows[0]["Año"]}' , '{dscar.Carrera.Rows[0]["NºResolucion"]}' , ");
+            }
+
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+                
+
+            }
+
+        public void EliminarPersona(string bNombre)
+        {
+            try
+            {
+                ConexionBD.StoredSinDatos("EliminarPersona", dscar.Carrera);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+           
+
+            }
+        }
+        }
+
+
+     
+
+
--- Sistema/FE/CargarCarrera.cs
+++ Sistema/FE/CargarCarrera.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
+using Sistema.BE;
 
 namespace Sistema.FE
 {
     public partial class CargarCarrera : Form
     {
+        BeCarrera BeC;
+
         public CargarCarrera()
         {
             InitializeComponent();
@@ -17,6 +20,62 @@ namespace Sistema.FE
 
         private void CargarCarrera_Load(object sender, EventArgs e)
         {
+            Nuevo();
+        }
+
+        private void Nuevo()
+        {
+            txtAños.Text = "";
+            txtCarrera.Text = "";
+            txtHoras.Text = "";
+            txtResolucion.Text = "";
+        
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            #region Validacion
+
+            if(txtCarrera.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Debe ingrsar el nombre de la Carrera");
+                return;
+            }
+            if(txtResolucion.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Debe ingresar el Nº de Resolucion");
+                return;
+            }
+
+            #endregion
+
+            BeC = new BeCarrera();
+            try
+            {
+                BeC.CargarCarrera(txtAños.Text, txtCarrera.Text, txtHoras.Text, txtResolucion, Text);
+                MessageBox.Show("Carrera Guardada");
+                Nuevo();
+                
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            BeC = new BeCarrera();
+            try
+            {
+                BeC.EliminarCarrera(txtCarrera.Text);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
         }
     }
