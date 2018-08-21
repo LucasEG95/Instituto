a6b7c702318ffabf064bc44039ccced1b54552ea
Rene Paolucci <renepao99@hotmail.com>
2018-08-21 19:34:43 -0300

    Ingreso en proceso

---
+++ Sistema/BE/BeIngreso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema.DAL;
+using System.Data.SqlClient;
+
+namespace Sistema.BE
+{
+    class BeIngreso
+    {
+        DalIngreso DalIng;
+        public int Login(string Usuario, string Contra)
+       {
+            DalIng = new DalIngreso;
+        try
+        {
+            return DalIng.
+        }
+        catch (Exception ex)
+        { 
+            throw ex;          
+        }
+
+}
+}
