1879605199b7f819933a01819664c8029a0f01af
skreamozz <41153479+skreamozz@users.noreply.github.com>
2018-10-22 21:18:38 -0300

    lalala

--- Sistema/FE/FormLupa.Designer.cs
+++ Sistema/FE/FormLupa.Designer.cs
@@ -32,7 +32,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cmbFiltro = new System.Windows.Forms.ComboBox();
             this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.GrillaLupa)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // GrillaLupa
@@ -41,21 +43,25 @@
             this.GrillaLupa.AllowUserToDeleteRows = false;
             this.GrillaLupa.AllowUserToResizeRows = false;
             this.GrillaLupa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.GrillaLupa.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.GrillaLupa.BackgroundColor = System.Drawing.SystemColors.Control;
             this.GrillaLupa.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.GrillaLupa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GrillaLupa.Location = new System.Drawing.Point(-1, -2);
+            this.GrillaLupa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GrillaLupa.Location = new System.Drawing.Point(0, 0);
             this.GrillaLupa.Name = "GrillaLupa";
             this.GrillaLupa.ReadOnly = true;
             this.GrillaLupa.RowHeadersVisible = false;
-            this.GrillaLupa.Size = new System.Drawing.Size(372, 387);
+            this.GrillaLupa.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.GrillaLupa.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.GrillaLupa.Size = new System.Drawing.Size(373, 374);
             this.GrillaLupa.TabIndex = 0;
             this.GrillaLupa.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GrillaLupa_CellDoubleClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 394);
+            this.label1.Location = new System.Drawing.Point(6, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -64,7 +70,7 @@
             // cmbFiltro
             // 
             this.cmbFiltro.FormattingEnabled = true;
-            this.cmbFiltro.Location = new System.Drawing.Point(53, 391);
+            this.cmbFiltro.Location = new System.Drawing.Point(47, 11);
             this.cmbFiltro.Name = "cmbFiltro";
             this.cmbFiltro.Size = new System.Drawing.Size(154, 21);
             this.cmbFiltro.TabIndex = 2;
@@ -72,27 +78,41 @@
             // 
             // txtFiltro
             // 
-            this.txtFiltro.Location = new System.Drawing.Point(213, 391);
+            this.txtFiltro.Location = new System.Drawing.Point(207, 11);
             this.txtFiltro.Name = "txtFiltro";
             this.txtFiltro.Size = new System.Drawing.Size(145, 20);
             this.txtFiltro.TabIndex = 3;
             this.txtFiltro.TextChanged += new System.EventHandler(this.txtFiltro_TextChanged);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbFiltro);
+            this.groupBox1.Controls.Add(this.txtFiltro);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(0, 380);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 38);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
             // FormLupa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 416);
-            this.Controls.Add(this.txtFiltro);
-            this.Controls.Add(this.cmbFiltro);
-            this.Controls.Add(this.label1);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(373, 418);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.GrillaLupa);
             this.Name = "FormLupa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormLupa";
             this.Load += new System.EventHandler(this.FormLupa_Load);
             ((System.ComponentModel.ISupportInitialize)(this.GrillaLupa)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -102,5 +122,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cmbFiltro;
         private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
